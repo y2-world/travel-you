89772b21ece088b6a2bc9727f462285b2752a1ff
y2world <yui92496@gmail.com>
2021-01-30 17:27:04 +0900

    Ver 2.7.1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 <div class="row">
   <div class="col-sm-4">
   <div class="card-header">
-        <h4>OUR TRAVELS</h4>
+        <h4>OUR TRAVEL DIARIES</h4>
   </div>
     <div class="card" style="width: 22rem;">
         @foreach ($posts as $post)
@@ -16,6 +16,9 @@
           <h5 class="card-title">{{ $post->title }}</h5>
           <p class="card-text"> {{ $post->date1 }} 〜 {{ $post->date2 }}</p>
           by&nbsp;<a href="{{ route('users.show', $post->user_id)}}">{{ $post->user_name }}</a>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">{{ $post->updated_at }}</small>
         </div>
         @endforeach
     </div>
@@ -29,7 +32,10 @@
         <div class="card-body">
           <h5 class="card-title">スペインのAirbnbでは鍵に要注意！</h5>
           <p class="card-text"></p>
-          by&nbsp;<a href="{{ route('users.show', $post->user_id)}}"></a>
+          by Yuki
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
         </div>
     </div>
   </div>
@@ -40,13 +46,14 @@
     <div class="card" style="width: 22rem;">
         <div class="card-body">
           <h5 class="card-title">2月にスペインに行く予定なのですが、寒いですか?</h5>
-          by&nbsp;<a href="{{ route('users.show', $post->user_id)}}"></a>
+          by Mami
           <hr>
-          <h5 class="card-title">さんの回答</h5>
+          <h5 class="card-title">Yukiさんの回答</h5>
           <p class="card-text">2月にスペインのバルセロナにに行ったことがありますが、暖かかったです。20℃くらいありました。</h5>
-          <br>
-          <br>
         </div>  
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
     </div>
   </div>
 </div>
