ee9a07d7a603e21ea1e28a1d14c27e1466fdb4fb
y2world <yui92496@gmail.com>
2021-02-01 19:30:00 +0900

    Ver 3.1

--- resources/views/troubles/create.blade.php
+++ resources/views/troubles/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
         <h2 class="trouble">TROUBLE×YOU</h2>
