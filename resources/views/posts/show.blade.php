7bea2189d0ff87eb97f72bffd9c548ed2770272c
y2world <yui92496@gmail.com>
2021-02-03 01:34:35 +0900

    Ver 4.3

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,36 +1,38 @@
 @extends('layouts.app')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-sm-8">
-         <div class="card-body">
-            <h4>{{ $post->title }}</h4>
-            <p class="card-date">{{ $post->date1 }}&nbsp;〜&nbsp;{{ $post->date2 }}</p>
-            <p class="card-category">by&nbsp;<a href="{{ route('users.show', $post->user_id)}}">{{ $post->user_name }}</a>
-        </div>
-        <li class="list-group-item">
-            <img src="{{ asset('storage/' . $post->image) }}" width="100%">
-            <br>
-            <br>
-            <p class="card-country">国 : {{ $post->country }}</p>
-            <p class="card-city">都市 : {{ $post->city }}</p>
-            <hr>
-            <h4 class="diary">TRAVEL DIARY</h4>
-            <p class="card-city">{{ $post->diary }}</p>
-            <hr>
-            <h4 class="diary">PHOTOS</h4>
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h2 class="trouble">TRAVEL DIARY</h2>
+        <div class="col-sm">
+        <hr>
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img class="card-img" src="{{ asset('storage/' . $post->image) }}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"><h5 class="card-title">{{ $post->title }}</h5></a></h5>
+                            <p class="card-category">by&nbsp;<a href="{{ route('users.show', $post->user_id)}}">{{ $post->user_name }}</a>
+                            <p>国 : {{ $post->country }}</p>
+                            <p>都市 : {{ $post->city }}</p>
+                            <hr>
+                            <p>{{ $post->diary }}</p>
+                            <hr>
+                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="{{ asset('storage/' . $post->image1) }}" width="100%">
+                            <img src="{{ asset('storage/' . $post->image1) }}" width="50%">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{ asset('storage/' . $post->image2) }}" width="100%">
+                            <img src="{{ asset('storage/' . $post->image2) }}" width="50%">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{ asset('storage/' . $post->image3) }}" width="100%">
+                            <img src="{{ asset('storage/' . $post->image3) }}" width="50%">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{ asset('storage/' . $post->image4) }}" width="100%">
+                            <img src="{{ asset('storage/' . $post->image4) }}" width="50%">
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -41,15 +43,16 @@
                             <span class="sr-only">Next</span>
                         </a>
                     </div>
+                    <br>
+                            <p>投稿日時：{{ $post->created_at }}</p> 
+                        <a href="{{ url('/posts') }}" class="btn btn-secondary">BACK</a>
+                        <br>
+                        </div>
+                    </div>
                 </div>
-            <br>
-            <hr>
-            <p>投稿日時：{{ $post->created_at }}</p> 
-            
-            <br>
-            <a href="{{ url('/posts') }}" class="btn btn-secondary">BACK</a>
-        </li>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-            
+</div>  
 @endsection
