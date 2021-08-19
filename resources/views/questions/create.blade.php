69d03fca1f6f3a7cc8f31267c19dd6ba74f6389c
y2-world <yui92496@gmail.com>
2021-08-19 15:30:42 +0900

    国一覧追加

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -21,18 +21,19 @@
                         <input type="text" class="form-control" placeholder="質問のタイトル" name="title">
                     </div>    
                     <div class="form-group">
-                        <label for="exampleFormControlSelect1">カテゴリー</label>&nbsp;<span class="badge bg-danger">必須</span>
-                        <select class="form-control" id="exampleFormControlSelect1" select name="category">
-                        <option value="基本情報">基本情報</option>
-                        <option value="チップ・マナー">チップ・マナー</option>
-                        <option value="宿泊先">宿泊先 (ホテル・Airbnbなど)</option>
-                        <option value="観光地">観光地</option>
-                        <option value="レストラン">レストラン</option>
-                        <option value="交通機関">交通機関</option>
-                        <option value="ショッピング">ショッピング</option>
-                        <option value="お土産">お土産</option>
-                        <option value="治安">治安</option>
-                        <option value="その他">その他</option>
+                        <label>カテゴリー</label>&nbsp;<span class="badge bg-danger">必須</span>
+                        <select class="form-control" select name="category">
+                            <option hidden>カテゴリーを選択</option>
+                            <option value="基本情報">基本情報</option>
+                            <option value="チップ・マナー">チップ・マナー</option>
+                            <option value="宿泊先">宿泊先 (ホテル・Airbnbなど)</option>
+                            <option value="観光地">観光地</option>
+                            <option value="レストラン">レストラン</option>
+                            <option value="交通機関">交通機関</option>
+                            <option value="ショッピング">ショッピング</option>
+                            <option value="お土産">お土産</option>
+                            <option value="治安">治安</option>
+                            <option value="その他">その他</option>
                         </select>
                     </div>   
                     <div class="form-group">
