9760502861c3edcb8e4b9de929aa911ce7b21196
y2-world <yui92496@gmail.com>
2021-09-09 20:21:18 +0900

    index width revised

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,30 +1,28 @@
 @extends('layouts.app')
 @section('content')
 <div class="page-wrapper">
-  <div class="index-container">
-    <div class="container-fluid">
-      <div class="container">
-        <h2 class="header-title">OUR TRAVELS　<a href="{{ url('posts/create') }}" class="btn btn-outline-primary" id="my_travels_botton">NEW POST</a></h2>
-        @if($posts->isEmpty())
-        <p>まだ投稿がありません。</p>
-        @endif 
-      </div>
-      <div class="cards">
-        @foreach ($posts as $post)
-        <div class="travel-card">
-          <p class="card-user"><a href="{{ route('users.show', $post->user_id)}}">{{ $post->user_name }}</a></p>
-          <p class="card-title">{{ $post->title }}</a></p>
-          <p class="card-date"> {{ $post->date1 }} 〜 {{ $post->date2 }}</p>
-          <div class="travel-img">
-            <a href="{{ route('posts.show', $post->id) }}"><img src="{{ asset('storage/' . $post->image) }}"></a>
-          </div>
-          <div class="card-info">
-            <p class="card-diary"> {{ $post->diary }}</p>
-            <p class="post-date">{{ $post->updated_at }}</p>
-          </div>
+  <div class="container-fluid">
+    <div class="container">
+      <h2 class="header-title">OUR TRAVELS　<a href="{{ url('posts/create') }}" class="btn btn-outline-primary" id="my_travels_botton">NEW POST</a></h2>
+      @if($posts->isEmpty())
+      <p>まだ投稿がありません。</p>
+      @endif 
+    </div>
+    <div class="cards">
+      @foreach ($posts as $post)
+      <div class="travel-card">
+        <p class="card-user"><a href="{{ route('users.show', $post->user_id)}}">{{ $post->user_name }}</a></p>
+        <p class="card-title">{{ $post->title }}</a></p>
+        <p class="card-date"> {{ $post->date1 }} 〜 {{ $post->date2 }}</p>
+        <div class="travel-img">
+          <a href="{{ route('posts.show', $post->id) }}"><img src="{{ asset('storage/' . $post->image) }}"></a>
         </div>
-        @endforeach
+        <div class="card-info">
+          <p class="card-diary"> {{ $post->diary }}</p>
+          <p class="post-date">{{ $post->updated_at }}</p>
+        </div>
       </div>
+      @endforeach
     </div>
   </div>
 </div>
--- resources/views/troubles/index.blade.php
+++ resources/views/troubles/index.blade.php
@@ -2,30 +2,28 @@
 @section('content')
 
 <div class="page-wrapper">
-    <div class="index-container">
-        <div class="container-fruid">
-            <div class="container">
-                <h2 class="header-title">TROUBLE×YOU　<a href="{{ url('troubles/create') }}" class="btn btn-outline-primary" id="my_travels_botton">NEW POST</a></h2>
-                @if($troubles->isEmpty())
-                <p>まだ投稿がありません。</p>
-                @endif 
-            </div>
-            <div class="cards">
-            @foreach ($troubles as $trouble)
-                <div class="travel-card">
-                    <p class="card-user"><a href="{{ route('users.show', $trouble->user_id)}}">{{ $trouble->user_name }}</a></p>
-                    <p class="card-title-trouble">{{ $trouble->title }}</a></p>
-                    <div class="travel-img">
-                        <a href="{{ route('troubles.show', $trouble->id) }}"><img src="{{ asset('storage/' . $trouble->image) }}"></a>
-                    </div>
-                    <p class="card-diary">{{Str::limit($trouble->content, 90, '…' )}}</p>
-                    <div class="trouble-row">
-                        <p class="post-date">{{ $trouble->updated_at }}</p>
-                        <div class="trouble-more"><a class="btn btn-primary btn-sm" href="{{ route('troubles.show', $trouble->id) }}">MORE</a></div>
-                    </div>
+    <div class="container-fruid">
+        <div class="container">
+            <h2 class="header-title">TROUBLE×YOU　<a href="{{ url('troubles/create') }}" class="btn btn-outline-primary" id="my_travels_botton">NEW POST</a></h2>
+            @if($troubles->isEmpty())
+            <p>まだ投稿がありません。</p>
+            @endif 
+        </div>
+        <div class="cards">
+        @foreach ($troubles as $trouble)
+            <div class="travel-card">
+                <p class="card-user"><a href="{{ route('users.show', $trouble->user_id)}}">{{ $trouble->user_name }}</a></p>
+                <p class="card-title-trouble">{{ $trouble->title }}</a></p>
+                <div class="travel-img">
+                    <a href="{{ route('troubles.show', $trouble->id) }}"><img src="{{ asset('storage/' . $trouble->image) }}"></a>
                 </div>
-            @endforeach
+                <p class="card-diary">{{Str::limit($trouble->content, 90, '…' )}}</p>
+                <div class="trouble-row">
+                    <p class="post-date">{{ $trouble->updated_at }}</p>
+                    <div class="trouble-more"><a class="btn btn-primary btn-sm" href="{{ route('troubles.show', $trouble->id) }}">MORE</a></div>
+                </div>
             </div>
+        @endforeach
         </div>
     </div>
 </div>    
